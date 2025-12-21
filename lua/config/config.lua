babf04da7607ee26523830ce97656eae1ef7a879
LGgameLAB <lukegonsalves07@gmail.com>
2025-12-21 13:51:42 -0800

    chore: update 21/25

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -3,3 +3,11 @@ local lspconfig = require("lspconfig")
 lspconfig.pyright.setup({
 	on_attach = on
 })
+require("neo-tree").setup({
+    window = {
+        mappings = {
+            ["<C-B>"] = function() vim.cmd("!sh run.sh") end,
+        },
+    },
+})
+
