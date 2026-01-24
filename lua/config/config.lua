0d60eca8d27c999508f1cd323edeb23c07a7c7de
LGgameLAB <lukegonsalves07@gmail.com>
2026-01-23 19:44:55 -0800

    fix: neotree closes when buffer closes

--- lua/config/config.lua
+++ lua/config/config.lua
@@ -8,11 +8,13 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		require("neo-tree").setup({
 			open_files_do_not_replace_types = { "terminal", "nofile" },
+            close_if_last_window = true,
 			filesystem = {
 				follow_current_file = {
 					enabled = true,
 				}, -- Keep Neo-tree focused on the current file
 				use_libuv_file_watcher = true, -- Automatically update tree
+                hijack_netrw_behavior = "open_current",
 			},
             window = {
                 width = 30,
